df0ee6e0a38161c2749e608e782fde2aa60e8f2a
Avii05 <avnendrakumar2001@gmail.com>
2022-04-09 12:44:50 +0530

    Commit

---
+++ P9_Duplicate Element.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Question 9"<<endl;
+	cout<<"Avnendra Singh Chauhan_F_200211224"<<endl;
+	int n,counter=0;
+	cout<<"Enter the size of array:";
+	cin>>n;
+	int arr[n];
+	
+}
